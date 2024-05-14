ef83a3540a8e4fe9287b9449caf51bfd9a6634dd
Guilherme Santana <guiguilsantana@gmail.com>
2024-05-13 22:31:27 -0300

    feat: Created Domain Place record

--- src/main/java/com/guilhermelopes/demo/domain/Place.java
+++ src/main/java/com/guilhermelopes/demo/domain/Place.java
@@ -1,84 +1,9 @@
 package com.guilhermelopes.demo.domain;
 
-import jakarta.annotation.Generated;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import org.springframework.cglib.core.Local;
+
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
-@Entity
-@Table(name = "places")
-public class Place {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String name;
-    private String lug;
-    private String state;
-    private LocalDateTime createdAt;
-    private LocalDateTime updateAt;
-
-    public Place() {
-    }
-
-    public Place(Long id, String name, String lug, String state, LocalDateTime createdAt, LocalDateTime updateAt) {
-        this.id = id;
-        this.name = name;
-        this.lug = lug;
-        this.state = state;
-        this.createdAt = createdAt;
-        this.updateAt = updateAt;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLug() {
-        return lug;
-    }
-
-    public void setLug(String lug) {
-        this.lug = lug;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(LocalDateTime updateAt) {
-        this.updateAt = updateAt;
-    }
+public record Place(@Id Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
 }
