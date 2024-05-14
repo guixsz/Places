7463aea2297ccd86a355ce89d1ca5b966c676602
Guilherme Santana <guiguilsantana@gmail.com>
2024-05-13 22:32:13 -0300

    feat: Created dto PlaceRequest and PlaceResponse

---
+++ src/main/java/com/guilhermelopes/demo/api/PlaceRequest.java
@@ -0,0 +1,6 @@
+package com.guilhermelopes.demo.api;
+
+import java.time.LocalDateTime;
+
+public record PlaceRequest(String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+}
---
+++ src/main/java/com/guilhermelopes/demo/api/PlaceResponse.java
@@ -0,0 +1,6 @@
+package com.guilhermelopes.demo.api;
+
+import java.time.LocalDateTime;
+
+public record PlaceResponse(String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+}
