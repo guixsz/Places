a69437703f8e5ccc8c7cf2c3712483ce5a16d941
Guilherme Santana <guiguilsantana@gmail.com>
2024-05-14 21:34:15 -0300

    feat: Automatic insertion of createdAt and updateAt

--- src/main/java/com/guilhermelopes/demo/domain/Place.java
+++ src/main/java/com/guilhermelopes/demo/domain/Place.java
@@ -1,9 +1,12 @@
 package com.guilhermelopes.demo.domain;
 
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
-public record Place(@Id Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record Place(@Id Long id, String name, String slug, String state,
+                    @CreatedDate LocalDateTime createdAt,@LastModifiedDate LocalDateTime updatedAt) {
 }
