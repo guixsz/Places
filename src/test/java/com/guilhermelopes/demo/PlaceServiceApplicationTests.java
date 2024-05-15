7009caa5226144ac9772abe3bfaa7256cc4d17a1
Guilherme Santana <guiguilsantana@gmail.com>
2024-05-14 21:36:24 -0300

    feat: Created Test success and Test failure

--- src/test/java/com/guilhermelopes/demo/PlaceServiceApplicationTests.java
+++ src/test/java/com/guilhermelopes/demo/PlaceServiceApplicationTests.java
@@ -1,13 +1,53 @@
 package com.guilhermelopes.demo;
 
+import com.guilhermelopes.demo.api.PlaceRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PlaceServiceApplicationTests {
+	@Autowired
+	WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testCreatePlaceSuccess() {
+		var name = "Valid Name";
+		var state = "Valid State";
+		var slug = "valid-name";
+
+		webTestClient
+				.post()
+				.uri("/places")
+				.bodyValue(
+						new PlaceRequest(name, state))
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody()
+				.jsonPath("name").isEqualTo(name)
+				.jsonPath("state").isEqualTo(state)
+				.jsonPath("slug").isEqualTo(slug)
+				.jsonPath("createdAt").isNotEmpty()
+				.jsonPath("updatedAt").isNotEmpty();
+	}
+
+	@Test
+	public void TestCreatePlaceFailure(){
+		var name = "";
+		var state = "";
+
+
+		webTestClient
+		.post()
+				.uri("/places")
+				.bodyValue(
+						new PlaceRequest(name, state))
+				.exchange()
+				.expectStatus().isBadRequest();
+
 	}
 
 }
