9cb3f9bcd7ed98c29fb531525724921afd215334
Guilherme Santana <guiguilsantana@gmail.com>
2024-05-14 21:34:46 -0300

    feat: Create Data Validation

--- src/main/java/com/guilhermelopes/demo/web/PlaceController.java
+++ src/main/java/com/guilhermelopes/demo/web/PlaceController.java
@@ -4,6 +4,7 @@ import com.guilhermelopes.demo.api.PlaceRequest;
 import com.guilhermelopes.demo.api.PlaceResponse;
 import com.guilhermelopes.demo.domain.Place;
 import com.guilhermelopes.demo.service.PlaceService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,7 @@ public class PlaceController {
         this.placeService = placeService;
     }
     @PostMapping
-    public ResponseEntity<Mono<PlaceResponse>> create(@RequestBody PlaceRequest request){
+    public ResponseEntity<Mono<PlaceResponse>> create(@Valid @RequestBody PlaceRequest request){
         var placeResponse = placeService.create(request).map(PlaceMapper::fromPlaceToResponse);
         return ResponseEntity.status(HttpStatus.CREATED).body(placeResponse);
     }
